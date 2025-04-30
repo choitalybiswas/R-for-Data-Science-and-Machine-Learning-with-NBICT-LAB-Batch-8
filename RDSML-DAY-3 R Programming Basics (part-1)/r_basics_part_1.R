c872dc485c78815ab1a84a35917c81f746389b9f
choitalybiswas <choitalybiswas@gmail.com>
2025-04-30 22:50:54 +0600

    RDSML Day -3 Code

---
+++ RDSML-DAY-3 R Programming Basics (part-1)/r_basics_part_1.R
@@ -0,0 +1,11 @@
+#commenting some health parameter
+
+my_age = 97 
+my_weighted = 60
+my_height = 65
+
+x = (my_height * my_age) / my_weighted
+print(x)
+
+students_height = c(50, 55, 64, 71, 45.25)
+mean(students_height)
